57ff4a66e744d42bccbba75cc5c69f7293b2ae6b
2no1 <diogo.ferreira.k23@gmail.com>
2017-08-12 17:34:46 +0100

    Update pass_gen.py

--- pass_gen.py
+++ pass_gen.py
@@ -1,79 +1,56 @@
+import argparse
 import random
+from argparse import RawTextHelpFormatter
+parser = argparse.ArgumentParser(description='Options are:\n1. Numbers only \n2. Lower letters only \n3.Upper letters only \n4. Lower letters and numbers \n5. Upper letters and numbers \n6. Upper and lower letters \n7. 100% random ', formatter_class=RawTextHelpFormatter)
+parser.add_argument("count", help="number of choices", type=int)
+parser.add_argument("length", help="length for passwd", type=int)
+parser.add_argument("-op", "--option", type=int)
+args = parser.parse_args()
 
-def numsonly():
-    c = int(input('number of choices: '))
-    l = int(input('length of string: '))
-    for _ in range(c):
-        qq = ''.join(random.choice("1234567890") for _ in range(l))
-        print('')
-        print(qq)
-def letterlow():
-    c = int(input('number of choices: '))
-    l = int(input('length of string: '))
+c = (args.count)
+l = (args.length)
+
+if args.option == 1:
+	for _ in range(c):
+		qq = ''.join(random.choice("1234567890") for _ in range(l))
+		print('')
+		print(qq)
+		print('')
+elif args.option == 2:
     for _ in range(c):
         qq = ''.join(random.choice("abcdefghijklmnopqrstuvwxyz") for _ in range(l))
         print('')
         print(qq)
-def letterup():
-    c = int(input('number of choices: '))
-    l = int(input('length of string: '))
+	print('')
+elif args.option == 3:
     for _ in range(c):
         qq = ''.join(random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ") for _ in range(l))
         print('')
         print(qq)
-def lnlow():
-    c = int(input('number of choices: '))
-    l = int(input('length of string: '))
+	print('')
+elif args.option == 4:
     for _ in range(c):
         qq = ''.join(random.choice("1234567890abcdefghijklmnopqrstuvwxyz") for _ in range(l))
         print('')
         print(qq)
-def lnup():
-    c = int(input('number of choices: '))
-    l = int(input('length of string: '))
+	print('')
+elif args.option == 5:
     for _ in range(c):
         qq = ''.join(random.choice("1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ") for _ in range(l))
         print('')
         print(qq)
-def lettersonly():
-    c = int(input('number of choices: '))
-    l = int(input('length of string: '))
+	print('')
+elif args.option == 6:
     for _ in range(c):
         qq = ''.join(random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") for _ in range(l))
         print('')
         print(qq)
-def super():   
-    c = int(input('number of choices: '))
-    l = int(input('length of string: '))
+	print('')
+elif args.option == 7:
     for _ in range(c):
         qq = ''.join(random.choice("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") for _ in range(l))
         print('')
         print(qq)
-       
-def menu():
-    print('1. numbers only')
-    print('2. letters only (lower case)')
-    print('3. letters only (upper case)')
-    print('4. letters(lower case) and numbers')
-    print('5. letters(upper case) and numbers')
-    print('6. letters(lower and upper case)')
-    print('7. 100% random')
-    op = input('Please choose: ')
-    if op == '1':
-        numsonly()
-    elif op == '2':
-        letterlow()
-    elif op == '3':
-        letterup()
-    elif op == '4':
-        lnlow()
-    elif op == '5':
-        lnup()
-    elif op == '6':
-        lettersonly()
-    elif op == '7':
-        super()
-    else:
-        print('WRONG!!')
-       
-menu()
+	print('')
+else:
+	print('WRONG!!')
